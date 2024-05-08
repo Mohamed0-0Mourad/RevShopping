a24bf4377ea4075cfb29fe4c45907023c734e17d
Mohamed Mourad <mohamedmoradmagdy1000@gmail.com>
2024-05-08 21:26:20 +0300

    dashboard function

--- Stats.py
+++ Stats.py
@@ -6,31 +6,32 @@ def avg(prices:list)->float:
     return sum(prices)/len(prices)
 
 def stats_dict(shopp_res: list[dict], weights: list ) -> dict:
-    src_price = dict()
-    src_discount= dict()
+    sources  = list()
+    prices = list()
+    discounts= list()
+    delivery = list()
     for i, res in enumerate(shopp_res):
         try:
+            src = res['source']
             price = res['extracted_price']
             old = res['extracted_old_prices']
-            src = res['source']
+            deliv= res['delivery']
         except KeyError:
             continue
         
         discount = old - price
 
-        try:
-            src_price[src].append(price)
-            src_discount[src].append(discount)
-        except KeyError:
-            src_price[src] = [price]
-            src_discount[src] = [discount]
-
-    for key in src_price:
-        src_price[key] = avg(src_price[key])
-        src_discount[key] = avg(src_discount[key])
-    df_dict = {"Sources": list(src_price.keys()), "Average Prices": list(src_price.values()), "Average Discounts": list(src_discount.values()), "Weight(rate*reviews)": weights}
+        sources.append(src)
+        prices.append(price)
+        discounts.append(discount)
+        delivery.append(deliv)
+    # for key in src_price:
+    #     src_price[key] = avg(src_price[key])
+    #     src_discount[key] = avg(src_discount[key])
+    df_dict = {"Position": range(1,len(shopp_res)),"Source": sources, "Price":prices, "Discount": discounts, "Trust(rate*reviews)": weights, "Delivery": delivery}
     return df_dict
 
 def dashboard(df_dict: dict):
     df = pd.DataFrame(df_dict)
-    
+    fig = px.bar(df, x= "Discount", y = "Price", width="Trust(rate*reviews)", color="Sources", facet_col="Delivery")
+    fig.show()
